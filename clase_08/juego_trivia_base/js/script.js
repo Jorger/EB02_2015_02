f6dba4c6725022b284bf3366e6ab23624fa60bde
Jorge Rubiano <jorge.rubiano@ymail.com>
2015-09-16 21:58:17 -0500

    Clase 08 - Base Juego Trivia
    Base del Juego Trivia Crack

---
+++ clase_08/juego_trivia_base/js/script.js
@@ -0,0 +1,29 @@
+$(function()
+{
+	preguntas = [];
+	var cargarJson = function()
+	{
+		var txtOpciones = "";
+		$.getJSON( "js/preguntas.json", function(data)
+		{
+			preguntas = data;
+			cargarPregunta(0);
+		});
+	}();
+
+	//Para cargar la pregunta...
+	var cargarPregunta = function(numPregunta)
+	{
+		$("#pregunta").html(preguntas[numPregunta].pregunta);
+		//Para cargar las opciones de respuesta...
+		for(var i = 1; i <= preguntas[numPregunta].opciones.length; i++)
+		{
+			$("#opcion_" + i).html(preguntas[numPregunta].opciones[i - 1])
+			.click(function(event) {
+				var ind = Number(this.id.split("_")[1]);
+				console.log(ind);
+			});
+
+		}
+	};
+});
